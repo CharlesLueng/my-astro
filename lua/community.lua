97dda6dcc3945568cb61d168194562794566af7b
CharlesLueng <charleslueng529@gmail.com>
2025-05-29 11:24:55 +0800

    初始化

---
+++ lua/community.lua
@@ -0,0 +1,35 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.json" },
+  -- { import = "astrocommunity.colorscheme.tokyodark-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.dracula-nvim"},
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim"},
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.mini-surround" },
+  -- { import = "astrocommunity.recipes.vscode" },
+  { import = "astrocommunity.completion.avante-nvim" },
+  { import = "astrocommunity.docker.lazydocker" },
+  -- { import = "astrocommunity.workflow.hardtime-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.recipes.vscode-icons" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = 'astrocommunity.pack.dart'}
+  -- import/override with your plugins folder
+}
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,45 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Mason
+
+---@type LazySpec
+return {
+  -- use mason-tool-installer for automatically installing Mason packages
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- overrides `require("mason-tool-installer").setup(...)`
+    opts = {
+      -- Make sure to use the names found in `:Mason`
+      ensure_installed = {
+        -- install language servers
+        "lua-language-server",
+
+        -- install formatters
+        "stylua",
+
+        -- install debuggers
+        "debugpy",
+
+        -- install any other package
+        "tree-sitter-cli",
+        "basedpyright",
+        "black",
+        "csharp-language-server",
+        "csharpier",
+        "eslint-lsp",
+        "isort",
+        "js-debug-adapter",
+        "json-lsp jsonls",
+        "netcoredbg",
+        "omnisharp",
+        "prettierd",
+        "pyright",
+        "selene",
+        "typescript-language-server",
+        "vtsls",
+        "vue-language-server",
+        "tailwindcss",
+      },
+    },
+  },
+}
